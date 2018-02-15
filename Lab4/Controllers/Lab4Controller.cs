2d50c19137783725d08cdb399fa874d3ad1fcfe0
Anthony J Dziedzic <dziedzi1@go.stockton.edu>
2018-02-14 20:50:57 -0500

    Add project files.

---
+++ Lab4/Controllers/Lab4Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var time = DateTime.Now;
+
+            return View(time);
+        }
+
+        public IActionResult Page2(int id)
+        {
+            if (id <= 5)
+                return View(id);
+            else
+                return View();
+        }
+
+        public IActionResult Page3()
+        {
+            return View();
+        }
+    }
+}
